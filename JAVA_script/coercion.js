325ebe73e27922560ade7c0b71f36f5bc5f8cfc8
Rashmi0609 <rashmi63867@gmail.com>
2025-10-14 11:55:47 +0530

    added coercion and loops in js

---
+++ JAVA_script/coercion.js
@@ -0,0 +1,25 @@
+console.log(5+"hello");
+console.log("5"-2);
+console.log("5"*2);
+console.log(1+true);
+console.log(2+false);
+console.log(Number("5"));
+console.log(String(100));
+console.log(Boolean(1)); 
+console.log(Number("abc"));
+"a".charCode
+"a".charCodeAt(0); 
+console.log("a".charCodeAt(0));
+String.fromCharCode(98);
+console.log(String.fromCharCode(98)); 
+console.log(2/0);
+console.log(-2/0); 
+console.log(0/0); //NaN
+console.log(NaN===NaN); //false
+console.log("a"-5);  //NaN
+console.log(typeof NaN); //number
+console.log(true+"5"); //true5
+console.log(true+5); //6b ,fc
+console.log("5"-true); //5true
+console.log(true+false); //5
+console.log(null+1); //5
---
+++ JAVA_script/loops.js
@@ -0,0 +1,33 @@
+let a=67;
+if(a==67){
+    console.log(`True`);
+}
+else{
+    console.log(`False`);
+}
+
+// largert b/w 2 numbers
+let b=prompt("Enter a Number: ");
+if(b<10){
+    console.log(`Number is less than 10`);
+}
+else if(b<20){
+    console.log(`Number is less than 20`);
+}
+else{
+    console.log(`Number is greater than 20`);
+}
+
+//3 print largest among 3 numbers
+let x=prompt("Enter first number: ");
+let y=prompt("Enter second number: ");
+let z=prompt("Enter third number: ");
+if(x>=y && x>=z){
+    console.log(`${x} is the largest number`);
+}   
+else if(y>=x && y>=z){
+    console.log(`${y} is the largest number`);
+}
+else{
+    console.log(`${z} is the largest number`);
+}
