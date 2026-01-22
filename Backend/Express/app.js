8d14e03be85e5edf6c098686808a7000115f9ba2
Rashmi0609 <rashmi63867@gmail.com>
2026-01-22 13:44:11 +0530

    express routes

---
+++ Backend/Express/app.js
@@ -0,0 +1,23 @@
+let express=require('express');
+let app=express();
+
+app.get('/',(req,res)=>{
+    res.send('This is / route');
+});
+
+app.get("/login",(req,res)=>{
+    res.send("This is login route");
+});
+
+app.get("/register",(req,res)=>{
+    res.send("This is register route");
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
+
+
+app.listen(8000,()=>{
+    console.log('app is running at port 8000');
+});
