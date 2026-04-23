1b108c06acdc70f942a01d1359eaf8338c69b00a
Rashmi0609 <rashmi63867@gmail.com>
2026-04-23 13:58:34 +0530

    authentication

--- Backend/JWT/app.js
+++ Backend/JWT/app.js
@@ -2,11 +2,39 @@ let express=require('express');
 let app=express();
 let jwt=require('jsonwebtoken');
 require('dotenv').config();
+app.use(express.json());
+let path=require('path');
+app.use(express.static(path.join(__dirname,'static')));  
 
-app.get('/login',(req,res)=>{
+let token='';
+
+app.get('/register',(req,res)=>{
     let {username,password}=req.query;
-    jwt.sign({"name":username, "Pass":password},process.env.SECRET);
+    token=jwt.sign({"name":username, "Pass":password},process.env.SECRET);
+    let payLoad=token.split('.')[1];
+    console.log(payLoad);
+    console.log('token generated');
+    res.send(token);
 })
+
+let authenticate=(req,res,next)=>{
+    jwt.verify(token,process.env.SECRET,(err,user)=>{
+        if(err){
+            res.send('invalid token');
+        }else{
+            console.log(user);
+            next();
+        }
+    })
+}
+
+app.get('/order',authenticate,(req,res)=>{
+    res.json({
+        "message":"invalid user"
+    })
+})  
+
+//http://localhost:3000/register?username=admin&password=admin123
 
 app.listen(process.env.port,()=>{
     console.log('server is running on port 3000')
